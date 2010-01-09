d27c3107c8426f246554eb2773e5bbc795fc90a3
Jorge Villatoro <programmerjorge@gmail.com>
2010-01-08 18:22:47 -0600

    Made some changes to the way the UARTs work, what with all the new DMA code

--- bob/boblib/uart/uart.c
+++ bob/boblib/uart/uart.c
@@ -10,29 +10,30 @@
 #include <pmc/pmc.h>
 #include <pio/pio.h>
 
+#include <dma/dma.h>
+
 #include <usb/device/cdc-serial/CDCDSerialDriver.h>
 #include <usb/device/cdc-serial/CDCDSerialDriverDescriptors.h>
 
 #define USB_BUFFER_SIZE \
         BOARD_USB_ENDPOINTS_MAXPACKETSIZE(CDCDSerialDriverDescriptors_DATAIN)
 
-char g_Uart0Buffer[UART_BUFFER_SIZE];
-char g_Uart1Buffer[UART_BUFFER_SIZE];
-
-char g_Uart1WriteBuffer[3][UART_BUFFER_SIZE];
-int g_Uart1WriteBufferIndex = 0;
-
-char g_UartUsbReadBuffer[USB_BUFFER_SIZE];
-char g_UartUsbWriteBuffer[USB_BUFFER_SIZE];
+// All of these are for the USB stuff
+unsigned char g_Uart0Buffer[UART_BUFFER_SIZE];
+unsigned char g_UartUsbReadBuffer[USB_BUFFER_SIZE];
+unsigned char g_UartUsbWriteBuffer[USB_BUFFER_SIZE];
 int  g_UartUsbWriteBufferBusy = 0;
-
 volatile int g_Uart0WriteIndex = 0;
 volatile int g_Uart0ReadIndex = 0;
 
-volatile int g_Uart1ReadIndex = 0;
-volatile int g_Uart1WriteIndex = 0;
+unsigned char g_Uart1Buffer[UART_BUFFER_SIZE];
+volatile int g_Uart1BufferIndex = 0;
+volatile int g_Uart1BufferReadIndex = 0;
+
+struct dma_dev g_Uart1DMA;
 
 volatile int g_UartUsbLock = 0;
+volatile int g_UartSigmask = 2;
 
 static const Pin g_UartPins[] = {SERIAL_TXD,SERIAL_RXD};
 
@@ -46,6 +47,7 @@ static void UartUsbWrite(void *pArg,
 												 unsigned int transferred,
 												 unsigned int remaining);
 static void UartRefresh();
+static void UpdateBend();
 
 int UartInit()
 {
@@ -54,17 +56,16 @@ int UartInit()
 	
 	UartStartRead();
 	
-	PMC_EnablePeripheral(AT91C_ID_US0);
 	PIO_Configure(g_UartPins, PIO_LISTSIZE(g_UartPins));
-	USART_Configure(AT91C_BASE_US0, AT91C_US_CHRL_8_BITS, 57600, MCK);
-	AT91C_BASE_PDC_US0->PDC_PTCR = AT91C_PDC_RXTEN;
+	PMC_EnablePeripheral(AT91C_ID_US0);
+	
+	USART_Configure(AT91C_BASE_US0, AT91C_US_USMODE_NORMAL | AT91C_US_CLKS_CLOCK | 
+																	AT91C_US_CHRL_8_BITS | AT91C_US_PAR_NONE | 
+																	AT91C_US_NBSTOP_1_BIT, 57600, MCK);
 	USART_SetReceiverEnabled(AT91C_BASE_US0, 1);
 	USART_SetTransmitterEnabled(AT91C_BASE_US0, 1);
 	
-	AT91C_BASE_PDC_US0->PDC_RPR  = (unsigned int)g_Uart1Buffer;
-	AT91C_BASE_PDC_US0->PDC_RNPR = (unsigned int)g_Uart1Buffer;
-	AT91C_BASE_PDC_US0->PDC_RCR  = UART_BUFFER_SIZE;
-	AT91C_BASE_PDC_US0->PDC_RNCR = UART_BUFFER_SIZE;
+	DMA_Init(&g_Uart1DMA, AT91C_BASE_PDC_US0);
 	
 	SetPitCallback(UartRefresh, 2);
 	
@@ -78,21 +79,44 @@ void UartStartRead()
 
 static void UartRefresh()
 {
-	g_Uart1WriteIndex = UART_BUFFER_SIZE - AT91C_BASE_PDC_US0->PDC_RCR;
+	if(g_Uart1BufferIndex + 64 >= UART_BUFFER_SIZE) {
+		g_Uart1BufferIndex += DMA_Read(&g_Uart1DMA, &(g_Uart1Buffer[g_Uart1BufferIndex]), UART_BUFFER_SIZE-g_Uart1BufferIndex);
+	}
+	else g_Uart1BufferIndex += DMA_Read(&g_Uart1DMA, &(g_Uart1Buffer[g_Uart1BufferIndex]), 64);
 	
-	if(!AT91C_BASE_PDC_US0->PDC_RNCR) {
-		AT91C_BASE_PDC_US0->PDC_RNPR = (unsigned int)g_Uart1Buffer;
-		AT91C_BASE_PDC_US0->PDC_RNCR = UART_BUFFER_SIZE;
+	if(g_Uart1BufferIndex == UART_BUFFER_SIZE) {
+		g_Uart1BufferIndex = 0;
 	}
 	
-	if(g_Uart1WriteIndex != g_Uart1ReadIndex) {
-		ChumbyBend(1);
-	}
+	UpdateBend();
+}
+
+void UartSetSigmask(int mask)
+{
+	g_UartSigmask = mask;
+}
+
+int UartGetSigmask()
+{
+	return g_UartSigmask;
+}
+
+static void UpdateBend()
+{
+	int data_ready = 0;
+	
+	if(g_UartSigmask & 1 && g_Uart0ReadIndex != g_Uart0WriteIndex)
+		data_ready = 1;
+		
+	if(g_UartSigmask & 2 && g_Uart1BufferIndex != g_Uart1BufferReadIndex)
+		data_ready = 1;
+		
+	if(data_ready) ChumbyBend(1);
+	else ChumbyBend(0);
 }
 
 int UartWrite(int uart, char *data, int len)
 {
-	int i = 0;
 	if(uart == 0) {
 		if(g_UartUsbWriteBufferBusy)
 			return 0;
@@ -103,23 +127,7 @@ int UartWrite(int uart, char *data, int len)
 		return len;
 	}
 	else {
-		while(i < len) {
-			//printf("Writing %d\r\n", data[i]);
-			//USART_Write(AT91C_BASE_US0, data[i++], 0);
-		}
-		/*if(AT91C_BASE_PDC_US0->PDC_TCR != 0) {
-			if(AT91C_BASE_PDC_US0->PDC_TCR != 0)
-				return 0;
-			else
-				g_Uart1WriteBufferState ^= 1;
-		}
-		
-		memcpy(g_Uart1WriteBuffer[g_Uart1WriteBufferState], data, len);
-		AT91C_BASE_PDC_US0->PDC_TNPR = (unsigned int)g_Uart1WriteBuffer[g_Uart1WriteBufferState];
-		AT91C_BASE_PDC_US0->PDC_TNCR = len;
-		
-		printf("Wrote %d bytes\r\n", len);*/
-		return len;
+		return DMA_Write(&g_Uart1DMA, (void*)data, len);
 	}
 }
 
@@ -128,11 +136,7 @@ int UartWrite(int uart, char *data, int len)
 
 int UartRead(int uart, char *data, int len)
 {
-	if(g_Uart0ReadIndex == g_Uart0WriteIndex && g_Uart1ReadIndex == g_Uart1WriteIndex) {
-			ChumbyBend(0);
-			return 0;
-	}
-	
+	UpdateBend();
 	if(uart == 0) {
 		if(g_Uart0ReadIndex == g_Uart0WriteIndex) {
 			return 0;
@@ -149,27 +153,25 @@ int UartRead(int uart, char *data, int len)
 		memcpy(data, &(g_Uart0Buffer[g_Uart0ReadIndex]), len);
 		g_Uart0ReadIndex += len;
 		if(g_Uart0ReadIndex >= UART_BUFFER_SIZE) g_Uart0ReadIndex = 0;
+		UpdateBend();
 		return len;
 	}
-	else if(uart == 1) {
-		if(g_Uart1ReadIndex == g_Uart1WriteIndex) {
+	else if(uart == 1){
+		if(g_Uart1BufferReadIndex == g_Uart1BufferIndex)
 			return 0;
-		}
-		
-		if(g_Uart1WriteIndex < g_Uart1ReadIndex) {
-			if(UART_BUFFER_SIZE - g_Uart1ReadIndex < len)
-				len = UART_BUFFER_SIZE - g_Uart1ReadIndex;
+		if(g_Uart1BufferIndex < g_Uart1BufferReadIndex) {
+			if(UART_BUFFER_SIZE - g_Uart1BufferReadIndex < len)
+				len = UART_BUFFER_SIZE - g_Uart1BufferReadIndex;
 		}
 		else {
-			if(g_Uart1WriteIndex - g_Uart1ReadIndex < len)
-				len = g_Uart1WriteIndex - g_Uart1ReadIndex;
+			if(g_Uart1BufferIndex - g_Uart1BufferReadIndex < len)
+				len = g_Uart1BufferIndex - g_Uart1BufferReadIndex;
 		}
 		
-		memcpy(data, &(g_Uart1Buffer[g_Uart1ReadIndex]), len);
-		g_Uart1ReadIndex += len;
-		if(g_Uart1ReadIndex >= UART_BUFFER_SIZE) g_Uart1ReadIndex = 0;
-		
-		printf("Read %d bytes\r\n", len);
+		memcpy(data, &(g_Uart1Buffer[g_Uart1BufferReadIndex]), len);
+		g_Uart1BufferReadIndex += len;
+		if(g_Uart1BufferReadIndex >= UART_BUFFER_SIZE) g_Uart1BufferReadIndex = 0;
+		UpdateBend();
 		return len;
 	}
 	return 0;
