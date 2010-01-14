3861b71437499ad73b1f186be8c8aad5a4e2cce8
Jorge Villatoro <programmerjorge@gmail.com>
2010-01-13 18:37:37 -0600

    ChumbyBend is now ChumbyWiggle, removed the spurious ChumbySS1 line in UartUsbRead, and moved some USB Connection stuff out of the UartInit function

--- bob/boblib/uart/uart.h
+++ bob/boblib/uart/uart.h
@@ -8,7 +8,6 @@ int UartWrite(int uart, char *data, int len);
 int UartRead(int uart, char *data, int len);
 int UartReset(int uart);
 void UartSetSigmask(int mask);
-int  UartGetSigmask();
 void UartStartRead();
 
 #endif
